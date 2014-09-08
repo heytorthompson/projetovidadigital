862d972f5d18f58684927cc4dd9b92edd988a9e2
heytor padilha <heytorthompson@gmail.com>
2014-09-08 14:12:42 -0300

    start

---
+++ app/Controller/PrincipalController.php
@@ -0,0 +1,12 @@
+<?php
+
+
+App::uses('AppController', 'Controller');
+
+class PrincipalController extends AppController {
+
+
+	public function index() {
+
+	}
+}
---
+++ app/View/Principal/index.ctp
@@ -0,0 +1,3 @@
+
+<?php
+echo 'Projeto vida digital';
